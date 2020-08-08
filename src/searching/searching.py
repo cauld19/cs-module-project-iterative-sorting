32f01112422629e0eecf328f7b8d813e94f17cbf
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-08-08 10:05:23 -0700

    searching two ways

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -25,3 +25,19 @@ def binary_search(arr, target):
 
 
     return -1  # not found
+
+def binary_search(arr, target):
+
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
