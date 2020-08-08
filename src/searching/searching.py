6bced36e9e9cb4fb09e359f1384144b91065cd55
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-08-08 09:20:22 -0700

    linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,9 @@
 def linear_search(arr, target):
-    # Your code here
+    count = -1
+    for i in range(len(arr)):
+        count += 1
+        if arr[i] is target:
+            return count 
 
 
     return -1   # not found
